96309d514f07cd41acf69b8b8f4fa5056b5e50c0
2everef0x <Mike@Michals-MacBook-Pro.local>
2019-11-01 14:49:58 +0000

    initiall commit

---
+++ src/Palette.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+
+class Palette extends Component{
+
+    constructor(props){
+         super(props)
+
+    }
+
+    render() {
+
+        return(
+        <div className="Palette">
+        {/* navbar goes ere */}
+           <div className="Palette-colors">
+
+           </div>
+           {/* footer goes ere */}
+        </div>
+        )
+    }
+}
+
+export default Palette
